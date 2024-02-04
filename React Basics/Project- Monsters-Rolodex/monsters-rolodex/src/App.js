0c4447984163d3adc1a589d1dbdfb23a33c1c6b9
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-02-04 20:23:06 +0600

    Monsters Rolodex with Class Components Done

--- React Basics/Project- Monsters-Rolodex/monsters-rolodex/src/App.js
+++ React Basics/Project- Monsters-Rolodex/monsters-rolodex/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { Component } from "react"
+import CardList from "./Components/Card-List/CardList.component"
+import SearchBox from "./Components/Search-Box/SearchBox.component"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      searchField: "",
+      monsters: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(() => {
+          return {
+            monsters: users,
+          }
+        })
+      )
+  }
+
+  onChangeHandler = (event) => {
+    this.setState(() => {
+      return {
+        searchField: event.target.value,
+      }
+    })
+  }
+
+  render() {
+    const filteredMonstersList = this.state.monsters.filter((monster) => {
+      return monster.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase())
+    })
+
+    return (
+      <>
+      <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Enter Monster Name"
+          onChangeHandler={this.onChangeHandler}
+          className='monster-search-box'
+        />
+        <CardList monsters={filteredMonstersList} />
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
