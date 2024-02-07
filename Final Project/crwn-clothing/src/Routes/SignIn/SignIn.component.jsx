230d55ec80fe737ab83d465bd16027813d620f3e
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-02-07 08:25:25 +0600

    removed Google Redirect Sign-In methods - Done Exploring It

--- Final Project/crwn-clothing/src/Routes/SignIn/SignIn.component.jsx
+++ Final Project/crwn-clothing/src/Routes/SignIn/SignIn.component.jsx
@@ -1,24 +1,9 @@
-import { getRedirectResult } from "firebase/auth"
-import { useEffect } from "react"
 import {
-  auth,
   createUserFromAuth,
   singInWithGooglePopUp,
-  signInWithGoogleRedirect,
 } from "../../utils/Firebase-Utils/firebase.utils"
 
 const SignIn = () => {
-  useEffect(() => {
-    async function fetchData() {
-      const response = await getRedirectResult(auth)
-      if (response) {
-        createUserFromAuth(response.user)
-      }
-    }
-
-    fetchData()
-  }, [])
-
   const logGoogleUser = async () => {
     const { user } = await singInWithGooglePopUp()
     const userDocRef = await createUserFromAuth(user)
@@ -28,9 +13,6 @@ const SignIn = () => {
     <>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In with Google Pop Up</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign In with Google Redirect
-      </button>
     </>
   )
 }
