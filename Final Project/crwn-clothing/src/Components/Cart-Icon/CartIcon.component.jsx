17fccf18d6a010e2c75382175ee321e29f375490
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-02-16 14:40:36 +0600

    Cart Icon Done

--- Final Project/crwn-clothing/src/Components/Cart-Icon/CartIcon.component.jsx
+++ Final Project/crwn-clothing/src/Components/Cart-Icon/CartIcon.component.jsx
@@ -1,20 +1,25 @@
-import "./cart-icon.styles.scss"
+import "./cart-icon.styles.jsx"
 import CART_ICON from "../../assets/shopping-bag.svg"
 import { useContext } from "react"
 import { CartIconContext } from "../../Context/CartIcon.context"
+import {
+  CartIconContainer,
+  ItemCount,
+  ShoppingIcon,
+} from "./cart-icon.styles.jsx"
 
 const CartIcon = () => {
-
-  const {cartIconToggle,setCartIconToggle, cartCount} = useContext(CartIconContext)
-  const handleCartIconToggle = ()=>{
+  const { cartIconToggle, setCartIconToggle, cartCount } =
+    useContext(CartIconContext)
+  const handleCartIconToggle = () => {
     setCartIconToggle(!cartIconToggle)
   }
 
   return (
-    <div className="cart-icon-container" onClick={handleCartIconToggle}>
-      <img className="shopping-icon" src={CART_ICON} alt="cart-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+    <CartIconContainer onClick={handleCartIconToggle}>
+      <ShoppingIcon src={CART_ICON} alt="cart-icon" />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   )
 }
 
