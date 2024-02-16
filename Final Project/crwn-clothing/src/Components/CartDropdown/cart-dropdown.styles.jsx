28780f1b556109238b9d577f8f589bc83086f822
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-02-16 14:33:10 +0600

    Cart Item done

--- Final Project/crwn-clothing/src/Components/CartDropdown/cart-dropdown.styles.jsx
+++ Final Project/crwn-clothing/src/Components/CartDropdown/cart-dropdown.styles.jsx
@@ -3,7 +3,7 @@ import { Button } from "../Button/Buttom.component"
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
-  width: 240px;
+  width: 260px;
   height: 340px;
   display: flex;
   flex-direction: column;
@@ -11,7 +11,7 @@ export const CartDropdownContainer = styled.div`
   border: 1px solid black;
   background-color: white;
   top: 90px;
-  right: 40px;
+  right: 50px;
   z-index: 5;
 `
 
