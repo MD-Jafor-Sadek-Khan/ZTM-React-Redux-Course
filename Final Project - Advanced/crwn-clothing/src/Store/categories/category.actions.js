82d96aac8bf3615d7e5ddaf8a81e5d6f9c454991
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-03-10 21:38:33 +0600

    ok

--- Final Project - Advanced/crwn-clothing/src/Store/categories/category.actions.js
+++ Final Project - Advanced/crwn-clothing/src/Store/categories/category.actions.js
@@ -1,6 +1,6 @@
 import { createAction } from "../../utils/Reducer-Utils/reducer.utils"
 import { CATEGORIES_ACTIONS_TYPES } from "./category.types"
 
-export const setCategoriesMap = (categoriesMap) =>{
+export const setCategoryMap = (categoriesMap) =>{
     return createAction(CATEGORIES_ACTIONS_TYPES.Set_Categories_Map, categoriesMap)
 }
