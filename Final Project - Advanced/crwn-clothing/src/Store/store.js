85a465c837fcbfb7f77219449a3a72e272fbaddc
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-03-12 16:25:01 +0600

    Implimented Redux-Persist for data hydration and Re-Hydration

--- Final Project - Advanced/crwn-clothing/src/Store/store.js
+++ Final Project - Advanced/crwn-clothing/src/Store/store.js
@@ -2,7 +2,12 @@ import { compose, createStore, applyMiddleware } from "redux"
 // import logger from "redux-logger"
 import { rootReducer } from "./root-reducer"
 
-const loggerMiddleware = store => next => action => {
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
+
+
+const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
     return next(action)
   }
@@ -21,4 +26,15 @@ const middlewares = [loggerMiddleware]
 
 const composeEnhancers = compose(applyMiddleware(...middlewares))
 
-export const store = createStore(rootReducer, undefined, composeEnhancers)
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) 
+
+
+export const store = createStore(persistedReducer, undefined, composeEnhancers)
+
+export const persistor = persistStore(store)
