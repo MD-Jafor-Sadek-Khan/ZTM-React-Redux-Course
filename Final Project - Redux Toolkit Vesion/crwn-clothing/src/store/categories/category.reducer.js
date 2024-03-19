496c2d2944a0c4f6055a8b6063a7df3d35de0b67
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-03-19 13:43:50 +0600

    Categories migration to Redux-toolkit done

--- Final Project - Redux Toolkit Vesion/crwn-clothing/src/store/categories/category.reducer.js
+++ Final Project - Redux Toolkit Vesion/crwn-clothing/src/store/categories/category.reducer.js
@@ -1,19 +1,33 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { createSlice } from "@reduxjs/toolkit"
+import { CATEGORIES_ACTION_TYPES } from "./category.types"
 
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
-};
+}
 
-export const categoriesReducer = (
-  state = CATEGORIES_INITIAL_STATE,
-  action = {}
-) => {
-  const { type, payload } = action;
+// export const categoriesReducer = (
+//   state = CATEGORIES_INITIAL_STATE,
+//   action = {}
+// ) => {
+//   const { type, payload } = action
 
-  switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categories: payload };
-    default:
-      return state;
-  }
-};
+//   switch (type) {
+//     case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+//       return { ...state, categories: payload }
+//     default:
+//       return state
+//   }
+// }
+
+export const categorySlice = createSlice({
+  name: "category",
+  initialState: CATEGORIES_INITIAL_STATE,
+  reducers: {
+    setCategories: (state, action) => {
+      state.categories = action.payload
+    },
+  },
+})
+
+export const categoriesReducer = categorySlice.reducer
+export const {setCategories} = categorySlice.actions
