e8d450678a9bb17bf6b82ee3ad4897fc8db10da9
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-03-19 13:34:44 +0600

    Converted users reducer action and type from redux to redux toolkit

--- Final Project - Redux Toolkit Vesion/crwn-clothing/src/utils/firebase/firebase.utils.js
+++ Final Project - Redux Toolkit Vesion/crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyD4f9MwlbsNNWR_MGRktRrexiXNvBViI6w",
+  authDomain: "crwn-clothing-db-304ab.firebaseapp.com",
+  projectId: "crwn-clothing-db-304ab",
+  storageBucket: "crwn-clothing-db-304ab.appspot.com",
+  messagingSenderId: "86324865595",
+  appId: "1:86324865595:web:ab6dd119179df691708a1e",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
